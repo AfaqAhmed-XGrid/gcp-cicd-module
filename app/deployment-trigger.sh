9aa322702a32172746a4cf3dcb16728e4ea1b42a
afaqahmed-XGrid <afaq.ahmed@xgrid.co>
2024-10-21 19:06:15 +0500

    updated sending image tag and project_id to deployer pipeline

--- app/deployment-trigger.sh
+++ app/deployment-trigger.sh
@@ -3,6 +3,6 @@
 PROJECT_ID="$1"
 API_KEY="$2"
 WEBHOOK_SECRET="$3"
-SHORT_SHA="$4"
+CONTAINER_TAG="$4"
 
-curl -XPOST "https://cloudbuild.googleapis.com/v1/projects/$PROJECT_ID/triggers/cloud-systems-app-ui-deployer:webhook?key=$API_KEY&secret=$WEBHOOK_SECRET" -d '{ "artifact": "'$SHORT_SHA'" }'
+curl -XPOST "https://cloudbuild.googleapis.com/v1/projects/$PROJECT_ID/triggers/cloud-systems-app-ui-deployer:webhook?key=$API_KEY&secret=$WEBHOOK_SECRET" -d '{ "artifact": "'$CONTAINER_TAG'", "project_id": "'$PROJECT_ID'" }'
